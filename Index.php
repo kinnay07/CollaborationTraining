c800c0fa4541ea597f238bc6fa9c1789523d567d
kinnay07 <101326590+kinnay07@users.noreply.github.com>
2022-03-10 18:09:34 +0700

    Delete Index.php

--- Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
-echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>FIRMAN DHANI PRATAMA (195610061)</h3>"
-
-?>
